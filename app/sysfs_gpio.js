44edb415d6a60d8fe04cfa1fb4d3c4ca7eee4d37
root <david6miji@gmail.com>
2016-09-02 14:05:22 +0000

    modifty this

--- app/sysfs_gpio.js
+++ app/sysfs_gpio.js
@@ -5,121 +5,102 @@ var
 end_require= true;
 
 const sysfs_base = '/sys/class/gpio/';
+const OFF  = 0;
+const ON   = 1;
 
-var append_gpio = function (index) {
-	console.log('CALL append_gpio');
 
-	return new Promise(function (resolve, reject) {
-	console.log('P CALL append_gpio');
-		fs.writeFile(sysfs_base + 'export', index, (err) => {
-			if (err) {
-				console.log('appending gpio['+index+'] is fail');
-				console.log(err);
-				reject(err);
-			} else {	
-				console.log('appending gpio['+index+'] is successed!');
-				resolve();
-			}
-		});
+sysfsGPIO = function(index){
+	console.log( "create sysfsGPIO("+index+")" );
+	this.index = index;
+	this.path  = sysfs_base + 'gpio' + index + '/';
+	
+ 	              this.remove_gpio()
+ 	.then(	() => this.append_gpio() )
+ 	.then(	() => this.output_mode_gpio() )
+ 	.then(	() => this.output_value_gpio( OFF ) );
 
-	});
 };
 
-var remove_gpio = function (index) {
-	console.log('CALL remove_gpio');
-
-	return new Promise(function (resolve, reject) {
-	console.log('P CALL remove_gpio');
-		fs.writeFile(sysfs_base + 'unexport', index, (err) => {
+sysfsGPIO.prototype.remove_gpio = function () {
+	return new Promise( (resolve, reject) => {
+		fs.writeFile(sysfs_base + 'unexport', this.index, (err) => {
 			if (err) {
 				if( err.code !== 'EINVAL' ) {
-					console.log('remove gpio['+index+'] is fail');
+					console.log('remove gpio['+this.index+'] is fail');
 					console.log(err);
 					reject(err);
 					return;
 				} 
 			}
-			
-			console.log('remove gpio['+index+'] is successed!');
+			console.log('remove gpio['+this.index+'] is successed!');
 			resolve();
 		});
 
 	});
 };
 
-var output_mode_gpio = function (path,index) {
-	console.log('CALL output_mode_gpio');
-
-	return new Promise(function (resolve, reject) {
-	console.log('P CALL output_mode_gpio');
-
-		console.log( path + 'direction' );
-		fs.writeFile( path + 'direction', 'out', (err) => {
+sysfsGPIO.prototype.append_gpio = function () {
+	return new Promise( (resolve, reject) => {
+		fs.writeFile(sysfs_base + 'export', this.index, (err) => {
 			if (err) {
-				console.log('changing gpio['+index+'] mode to output is failed');
+				console.log('appending gpio['+this.index+'] is fail');
+				console.log(err);
+				reject(err);
+			} else {	
+				console.log('appending gpio['+this.index+'] is successed!');
+				resolve();
+			}
+		});
+	});
+};
+
+sysfsGPIO.prototype.output_mode_gpio = function () {
+	return new Promise( (resolve, reject) => {
+		fs.writeFile( this.path + 'direction', 'out', (err) => {
+			if (err) {
+				console.log('changing gpio['+this.index+'] mode to output is failed');
 				console.log(err); 
 				reject(err);
 				return;
 			}
-			
-			console.log('changing gpio['+index+'] mode to output is successed!');
+			console.log('changing gpio['+this.index+'] mode to output is successed!');
 			resolve();
 		});
 
 	});
 };
 
-var output_value_gpio = function (path,index,value) {
-	console.log('CALL output_value_gpio');
-
-	return new Promise(function (resolve, reject) {
-	console.log('P CALL output_value_gpio');
-
-		console.log( path + 'value' );
-		fs.writeFile( path + 'value', value, (err) => {
+sysfsGPIO.prototype.output_value_gpio = function (value) {
+	return new Promise( (resolve, reject) => {
+		fs.writeFile( this.path + 'value', value, (err) => {
 			if (err) {
-				console.log('writing  gpio['+index+'] = ' + value + '] is failed');
+				console.log('writing  gpio['+this.index+'] = ' + value + '] is failed');
 				console.log(err); 
 				reject(err);
 				return;
 			}
-			
-			console.log('writing  gpio['+index+'] = ' + value + '] is successed!');
+			console.log('writing  gpio['+this.index+'] = ' + value + '] is successed!');
 			resolve();
 		});
 
 	});
 };
 
-
-var delay = function (msec) {
+sysfsGPIO.prototype.delay = function (msec) {
 	return new Promise( function (resolve, reject) {
-		console.log( "CALL delay("+msec+")" );  
 		setTimeout( function () {
-			console.log( "CALL delay timeout");  
 			resolve();
 		}, msec );
 	});
 };
 
-sysfsGPIO = function(index){
-	console.log( "CALL sysfsGPIO("+index+")" );
-	this.index = index;
-	this.path  = sysfs_base + 'gpio' + index + '/';
-	
- 	             remove_gpio      (this.index)
- 	.then(	p => append_gpio      (this.index) )
-//	.then(  p => delay            ( 5000 ) )
- 	.then(	p => output_mode_gpio (this.path, this.index) )
- 	.then(	p => output_value_gpio (this.path, this.index , 0 ) );
 
-};
+
 
 sysfsGPIO.prototype.test = function () {
 	var self = this;
 
 	console.log( 'CALL sysfsGPIO.test()' );
-	// 에러 처리를 나중에 추가 한다.
 
 	return self;
 }
