d423710af5972a38de023201e3d3e2cff2aade57
root <david6miji@gmail.com>
2016-09-03 07:11:21 +0000

    append check state

--- app/public/js/ctrl-main.js
+++ app/public/js/ctrl-main.js
@@ -3,8 +3,8 @@
 
 angular.module('hipoApp')
 
-.controller('mainCtrl', 
-function( $scope, $http ) {
+.controller('mainCtrl',
+function( $scope, $http, $interval ) {
 	console.log( 'CALL mainCtrl' );
 
 	$scope.forward_time  = 100;
@@ -12,32 +12,35 @@ function( $scope, $http ) {
 	$scope.backward_time  = 100;
 	$scope.backward_count = 10;
 
+	$scope.forward_rest_count  = 0;
+	$scope.backward_rest_count = 0;
+
 	$scope.goStop = function() {
 		console.log( "Stop" );
-		
+
 		$http({
-			url: "/control/stop", 
+			url: "/control/stop",
 			method: "GET",
 		}).	success( function(data){
 			console.log( "CALL API /control/stop" );
 		});
 	}
-	
+
 	$scope.goForward = function() {
 		console.log( "Forward" );
 		$http({
-			url: "/control/forward", 
+			url: "/control/forward",
 			method: "GET",
 			params: { time: $scope.forward_time , count : $scope.forward_count }
 		}).	success( function(data){
 			console.log( "CALL API /control/forward" );
 		});
 	}
-	
+
 	$scope.goBackward = function() {
 		console.log( "Backward" );
 		$http({
-			url: "/control/backward", 
+			url: "/control/backward",
 			method: "GET",
 			params: { time: $scope.backward_time , count : $scope.backward_count }
 		}).	success( function(data){
@@ -46,9 +49,23 @@ function( $scope, $http ) {
 
 	}
 
+	$interval( function () {
+//		console.log( "CALL ctrl-Main loop");
+		$http({
+			url: "/control/state",
+			method: "GET",
+		}).	success( function(data){
+
+//			console.log( "CALL API /control/state" );
+//			console.log(data);
+
+			$scope.forward_rest_count  = data.forward_count;
+			$scope.backward_rest_count = data.backward_count;
+		});
+	}, 100 );
+
 })
 
 ;
 
 })();
-
--- app/routes/control.js
+++ app/routes/control.js
@@ -1,28 +1,28 @@
-var 
+var
 	express 	= require('express'),
 	router 		= express.Router(),
 	sfg 		= require(__dirname + '/../sysfs_gpio'),
-	
+
 end_require= true;
 
 const 	FORWARD_GPIO_INDEX 	= 37;
 const 	BACKWARD_GPIO_INDEX = 36;
- 
+
 var gpio_forward 	= new sfg( FORWARD_GPIO_INDEX 	);
 var gpio_backward 	= new sfg( BACKWARD_GPIO_INDEX 	);
 
 router.get('/stop', function(req, res, next) {
-	
+
 	gpio_forward.stop();
 	gpio_backward.stop();
-	
+
 	res.send('ok stop');
 });
 
 router.get('/forward', function(req, res, next) {
 	console.log( "req.query.time = ", req.query.time );
 	console.log( "req.query.count = ", req.query.count );
-	
+
 	gpio_backward.stop();
 	gpio_forward.start_pulse(req.query.time,req.query.count);
 
@@ -32,11 +32,23 @@ router.get('/forward', function(req, res, next) {
 router.get('/backward', function(req, res, next) {
 	console.log( "req.query.time = ", req.query.time );
 	console.log( "req.query.count = ", req.query.count );
-	
+
 	gpio_forward.stop();
 	gpio_backward.start_pulse(req.query.time,req.query.count);
-	
+
 	res.send('ok backward');
+});
+
+router.get('/state', function(req, res, next) {
+
+	var state = {};
+
+	state.result 			= 'ok';
+	state.forward_count 	= parseInt(gpio_forward.count/2);
+	state.backward_count 	= parseInt(gpio_backward.count/2);
+
+    res.json(state);
+
 });
 
 module.exports = router;
